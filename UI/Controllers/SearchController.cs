d2808b1468f533357d0623d880623f41323b369c
VincentLeng <1084993708@qq.com>
2018-05-09 11:12:22 +0800

    update searchIndex

--- UI/Controllers/SearchController.cs
+++ UI/Controllers/SearchController.cs
@@ -13,15 +13,25 @@ namespace UI.Controllers
         // GET: Search
         CategoriesBLL categoriesBLL = new CategoriesBLL();
         ActivitiesBLL activitiesBLL = new ActivitiesBLL();
-       
+        Models.SearchViewModel searchvm = new Models.SearchViewModel();
+
         public ActionResult Index()
         {           
             //string search = Session["Search"].ToString();      
-            var categories = categoriesBLL.GetDAL().GetList(3, 1);
-            var activities = activitiesBLL.GetDAL().GetList(3, 1);
+            var categories = categoriesBLL.GetDAL().GetList(5, 1);
+            var activities = activitiesBLL.GetDAL().GetList(5, 1);
             Models.SearchViewModel searchvm = new Models.SearchViewModel();        
             searchvm.Categories = categories; 
             searchvm.Activities = activities;
+            return View(searchvm);
+        }
+        //搜索页面
+        [HttpPost]
+        public ActionResult Index(string search)
+        {
+            Session["Search"] = search;
+            searchvm.Categories = categoriesBLL.GetList(5,1);
+            searchvm.Activities = activitiesBLL.GetList(5,1);
             return View(searchvm);
         }
     }
